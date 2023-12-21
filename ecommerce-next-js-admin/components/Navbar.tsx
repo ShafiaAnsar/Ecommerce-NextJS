7b65fab2da5623bbc95b8da623f10698c5770640
shafiaansar <shafiaansar142@gmail.com>
2023-12-21 08:44:52 +0500

    /

--- ecommerce-next-js-admin/components/Navbar.tsx
+++ ecommerce-next-js-admin/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import { UserButton } from "@clerk/nextjs"
+import { MainNav } from "./MainNav"
+import StoreSwitcher from "./store-switcher"
 
 const Navbar = () => {
   return (
     <div className="border-b ">
         <div className="flex h-16 items-center px-4">
-            <div className="">This will be a store switcer</div>
+            <StoreSwitcher/>
+            <MainNav className="mx-6"/>
+            <div className="ml-auto flex items-center space-x-4 ">
+              <UserButton afterSignOutUrl="/"/>
+            </div>
         </div>
     </div>
   )
