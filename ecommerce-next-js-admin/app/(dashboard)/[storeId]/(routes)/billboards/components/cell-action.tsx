d1005f4339f01ebf582534f026ec8007d3b52cd7
shafiaansar <shafiaansar142@gmail.com>
2023-12-26 15:02:16 +0500

    data table added

--- ecommerce-next-js-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ ecommerce-next-js-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,15 +1,48 @@
 'use client'
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import axios from "axios"
 import { BillboardColumn } from "./columns"
 import { Button } from "@/components/ui/button"
-import { Edit, MoreHorizontal } from "lucide-react"
+import { Edit, Copy,MoreHorizontal, Trash } from "lucide-react"
+import toast from "react-hot-toast"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import { AlertModal } from "@/components/modals/alert-modal"
 
 interface CellActionColumnProps{
     data:BillboardColumn
 }
 export const CellAction:React.FC<CellActionColumnProps> = ({data})=>{
+    const router = useRouter()
+    const params= useParams()
+    const [loading,setLoading] = useState(false)
+    const [open, setOpen] = useState(false)
+    const onCopy = (id:string)=>{
+        navigator.clipboard.writeText(id)
+        toast.success('Billboard Id copied to the clipboard.')
+    }
+    const onDelete = async () => {
+        try {
+          setLoading(true);
+          await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+          router.refresh()
+          toast.success('Billboard deleted.');
+        } catch (error: any) {
+          toast.error('Make sure you removed all categories using this billboard first.');
+        } finally {
+          setLoading(false);
+          setOpen(false);
+        }
+      }
     return (
+        <>
+        <AlertModal
+        isOpen={open}
+        onClose={()=>setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+        />
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant='ghost' className="h-8 w-8 p-0">
@@ -19,11 +52,22 @@ export const CellAction:React.FC<CellActionColumnProps> = ({data})=>{
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
                 <DropdownMenuLabel>
-                    <DropdownMenuItem>
+                <DropdownMenuItem onClick={()=> onCopy(data.id)}>
+                        <Copy className='mr-2 h-4 w-4' />
+                        Copy Id
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=> router.push(`/${params.storeId}/billboards/${data.id}`)}>
                         <Edit className='mr-2 h-4 w-4'/>
+                        Update
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=> setOpen(true)}>
+                        <Trash className='mr-2 h-4 w-4'/>
+                        Delete
                     </DropdownMenuItem>
                 </DropdownMenuLabel>
             </DropdownMenuContent>
         </DropdownMenu>
+        </>
     )
+    
 }
