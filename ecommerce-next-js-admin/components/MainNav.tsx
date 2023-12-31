14abbf6dcf431be6a95a70a54fe32aac25b6d7b0
shafiaansar <shafiaansar142@gmail.com>
2023-12-31 10:25:34 +0500

    lets build sizes page

--- ecommerce-next-js-admin/components/MainNav.tsx
+++ ecommerce-next-js-admin/components/MainNav.tsx
@@ -21,7 +21,12 @@ export function MainNav ({className,...props}:React.HTMLAttributes<HTMLElement>)
     {
       href:`/${params.storeId}/categories`,
       label:'Categories',
-      active :pathname === `/${params.storeId}/billboards`,
+      active :pathname === `/${params.storeId}/categories`,
+    },
+    {
+      href:`/${params.storeId}/sizes`,
+      label:'Sizes',
+      active :pathname === `/${params.storeId}/sizes`,
     },
     {
             href:`/${params.storeId}/settings`,
