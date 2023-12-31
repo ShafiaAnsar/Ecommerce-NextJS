49d7995b50dfa3750c55ec254015c5f4cb0f6cca
shafiaansar <shafiaansar142@gmail.com>
2023-12-31 16:58:29 +0500

    modify colors client

--- ecommerce-next-js-admin/app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
+++ ecommerce-next-js-admin/app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -9,13 +9,13 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { ApiList } from "@/components/ui/api-list";
 
-import { columns, SizeColumn } from "./columns";
+import { ColorColumn, columns } from "./columns";
 
-interface SizesClientProps {
-  data: SizeColumn[];
+interface ColorsClientProps {
+  data: ColorColumn[];
 }
 
-export const SizesClient: React.FC<SizesClientProps> = ({
+export const ColorsClient: React.FC<ColorsClientProps> = ({
   data
 }) => {
   const params = useParams();
@@ -24,16 +24,16 @@ export const SizesClient: React.FC<SizesClientProps> = ({
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Sizes (${data.length})`} description="Manage sizes for your products" />
-        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+        <Heading title={`Colors (${data.length})`} description="Manage colors for your products" />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
       <DataTable searchKey="name" columns={columns} data={data} />
-      <Heading title="API" description="API Calls for Sizes" />
+      <Heading title="API" description="API Calls for Colors" />
       <Separator />
-      <ApiList entityName="sizes" entityIdName="sizeId" />
+      <ApiList entityName="colors" entityIdName="colorId" />
     </>
   );
 };
