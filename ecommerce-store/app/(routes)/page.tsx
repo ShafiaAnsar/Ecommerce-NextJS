90493189af80cd15504cfafa3b395c93c84a7d9c
shafiaansar <shafiaansar142@gmail.com>
2024-01-07 08:01:41 +0500

    Building home page

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,14 +1,20 @@
 import getBillboards from "@/actions/getBillboards"
+import getProducts from "@/actions/getProducts"
+import ProductList from "@/components/ProductList"
 import Billboard from "@/components/ui/Billboard"
 import Container from "@/components/ui/container"
 
 export const revalidate = 0
 const HomePage =  async() => {
+  const products = await getProducts({isFeatured:true})
   const billboards = await getBillboards('9d7c169a-124e-429f-bab9-1be0b8bb80bd')
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data ={billboards}/> 
+      </div>
+      <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8c">
+        <ProductList title='Featured Products' items={products}/>
       </div>
     </Container>
   )
---
+++ ecommerce-store/components/ui/NoResults.tsx
@@ -0,0 +1,8 @@
+
+const NoResults = () => {
+  return (
+    <div className="flex items-center h-full w-full text-neutral">NoResults</div>
+  )
+}
+
+export default NoResults
