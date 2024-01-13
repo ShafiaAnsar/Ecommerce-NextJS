9349b6f0b84523c44c9ab283160dac8e23e16181
shafiaansar <shafiaansar142@gmail.com>
2024-01-13 18:06:54 +0500

    product list completed

--- ecommerce-store/components/ui/IconButton.tsx
+++ ecommerce-store/components/ui/IconButton.tsx
@@ -1,11 +1,15 @@
 import { cn } from "@/libs/utils"
+import { MouseEventHandler } from "react"
+interface IconButtonProps{
+  onClick?:MouseEventHandler<HTMLButtonElement> | undefined
+  icon:React.ReactElement
+  className?:string
+}
 
-
-const IconButton = () => {
-const onClick = () => {}
+const IconButton:React.FC<IconButtonProps> = ({onClick,icon,className}) => {
   return (
-    <button onClick={onClick} className={cn('rounded-full items-center flex justify-center bg-white border shadow-md p-2  hover:scale-110 transition',)}>
-        
+    <button onClick={onClick} className={cn('rounded-full items-center flex justify-center bg-white border shadow-md p-2  hover:scale-110 transition',className)}>
+        {icon}
     </button>
   )
 }
